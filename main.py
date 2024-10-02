bc4f877cd8ee5c657fad5cf4f66be77e36a118e6
muztishini <111636144+muztishini@users.noreply.github.com>
2024-10-02 15:56:00 +0500

    small commit

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def get_kittens():
 async def get_kitten(id: int):
     kitten = db.query(Kitten).filter(Kitten.id == id).first()
     if kitten is None:
-        return JSONResponse(status_code=404, content={"message": f"Котенок с id {id} не найден"})
+        return JSONResponse(status_code=404, content={"message": f"Информация о котенке с id {id} не найдена"})
     return kitten
 
 
@@ -42,7 +42,7 @@ async def create_kitten(kitten_in: KittenSchema):
     db.add(kitten)
     db.commit()
     db.refresh(kitten)
-    return {"message": "Котенок добавлен", "data": kitten}
+    return {"message": "Информация о котенке добавлена", "data": kitten}
 
 
 @app.put("/api/kittens/{id}")
@@ -50,18 +50,18 @@ async def update_kitten(id: int, kitten_in: KittenSchema):
     kitten_query = db.query(Kitten).filter(Kitten.id == id)
     kitten_new = kitten_query.first()
     if kitten_new is None:
-        return JSONResponse(status_code=404, content={"message": f"Котенок с id {id} не найден"})
+        return JSONResponse(status_code=404, content={"message": f"Информация о котенке с id {id} не найдена"})
     kitten_query.update(kitten_in.model_dump())
     db.commit()
     db.refresh(kitten_new)
-    return {"message": "Котенок обновлен", "data": kitten_new}
+    return {"message": "Информация о котенке обновлена", "data": kitten_new}
 
 
 @app.delete("/api/kittens/{id}")
 async def delete_kitten(id: int):
     kitten = db.query(Kitten).filter(Kitten.id == id).first()
     if kitten is None:
-        return JSONResponse(status_code=404, content={"message": f"Котенок с id {id} не найден"})
+        return JSONResponse(status_code=404, content={"message": f"Информация о котенке с id {id} не найдена"})
     db.delete(kitten)
     db.commit()
-    return {"message": f"Котенок с id {id} удален!"}
+    return {"message": f"Информация о котенке с id {id} удалена!"}
